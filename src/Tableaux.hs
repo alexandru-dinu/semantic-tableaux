8c17298d3fe4d262a8dc640bbca97fb6dafdb624
alexandru-dinu <alex.dinu07@gmail.com>
2018-10-11 12:47:13 +0300

    add explanations

--- src/Tableaux.hs
+++ src/Tableaux.hs
@@ -8,19 +8,31 @@ import Control.Monad (guard)
 
 import Prop
 
--- a tableaux is a tree with nodes = sets of propositions and their respective sat (i.e. !closed)
+-- a tableaux is a tree with nodes = sets of propositions and their respective sat (i.e. ~closed)
 type NodeType = (Set Phi, Bool)
 type Tableaux = Tree NodeType
 
 tableaux :: Phi -> Tableaux
 tableaux sp = (build . Set.singleton) sp
 
--- TODO: DOC!!!
+{-
+the tree is built as follows:
+check the closedStatus of current set of formulas (sp)
+    if the set contains only atomic elements, 
+    construct a leaf node containing sp and its status (sat <-> ~closed)
+
+    if the set contains non-atomic elements,
+    select such an element (m) from the non-atomic-only subset,
+    delete it from the original sp (-> sp'),
+    and recursively construct the rest of the tree, branching on m
+    (depending on what formula m is)
+    the closedStatus of current node is an and of the all child statuses
+-}
 build :: Set Phi -> Tableaux
-build sp = case closed sp of
-    -- closed from the beginning, leaf, we're done
-    Just bool -> Tree.Node (sp, bool) [] 
-    -- not closed, construct tree
+build sp = case closedStatus sp of
+    -- sp is atomic -> leaf and its 'closed' status
+    Just cls -> Tree.Node (sp, cls) [] 
+    -- sp is non-atomic -> select + expand formula
     Nothing   -> Tree.Node (sp, cls) rest
     where
         (nonAtoms, _) = Set.partition (not . isAtom) sp
@@ -29,7 +41,8 @@ build sp = case closed sp of
         rest = map (build . Set.union sp') (branchOn m)
         cls  = and $ map isNodeClosed rest
 
--- Var checks
+
+-- atom checks
 isAtom :: Phi -> Bool
 isAtom (Var _)       = True
 isAtom (Not (Var _)) = True
@@ -57,32 +70,37 @@ existsComplements sp
 isNodeClosed :: Tableaux -> Bool
 isNodeClosed (Tree.Node (_, c) _) = c
 
--- if all are atoms in set, then check if there are complements
--- guard returns pure () if True, else empty
-closed :: Set Phi -> Maybe Bool
-closed sp = do
+{-
+Nothing -> there are non-atomic elements in the set 
+(i.e. they can be further expanded)
+Just cls -> if all elements are atomic, check for complements
+(i.e. cls == True <-> ∃ p, ~p in the set)
+-}
+closedStatus :: Set Phi -> Maybe Bool
+closedStatus sp = do
     guard $ allAtoms sp
     return $ existsComplements sp
 
--- ops: split / goWith
-split :: [Phi] -> [Set Phi]
-split xs = map Set.singleton xs
 
-goWith :: [Phi] -> [Set Phi]
-goWith xs = [Set.fromList xs]
+-- ops: separate / extract
+separate :: [Phi] -> [Set Phi]
+separate xs = map Set.singleton xs
+
+extract :: [Phi] -> [Set Phi]
+extract xs = [Set.fromList xs]
 
 branchOn :: Phi -> [Set Phi]
 -- no ramification, just _expand_ the formula
-branchOn (And p q)       = goWith [p, q]
-branchOn (Not (Or p q))  = goWith [Not p, Not q]
-branchOn (Not (Imp p q)) = goWith [p, Not q]
-branchOn (Iff p q)       = goWith [Imp p q, Imp q p]
-branchOn (Not (Not p))   = goWith [p]
+branchOn (And p q)       = extract [p, q]
+branchOn (Not (Or p q))  = extract [Not p, Not q]
+branchOn (Not (Imp p q)) = extract [p, Not q]
+branchOn (Iff p q)       = extract [Imp p q, Imp q p]
+branchOn (Not (Not p))   = extract [p]
 -- ramification
-branchOn (Or p q)        = split [p, q]
-branchOn (Imp p q)       = split [Not p, q]
-branchOn (Not (And p q)) = split [Not p, Not q]
-branchOn (Not (Iff p q)) = split [Not (Imp p q), Not (Imp q p)]
+branchOn (Or p q)        = separate [p, q]
+branchOn (Imp p q)       = separate [Not p, q]
+branchOn (Not (And p q)) = separate [Not p, Not q]
+branchOn (Not (Iff p q)) = separate [Not (Imp p q), Not (Imp q p)]
 
 
 -- eye-candy
